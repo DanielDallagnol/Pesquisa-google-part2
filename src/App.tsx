287e7b6f3e673907d4945090cb1bcb870812ef72
Daniel Dallagnol <danieldallagnol9@gmail.com>
2025-05-14 21:17:18 -0300

    fix-buidl-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import CriaForm from './assets/Form'
 import { Box } from '@mui/material'
@@ -30,6 +27,7 @@ function App() {
       <CriaForm/>
       
   </Box>
+
     </>
   )
 }
